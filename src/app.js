5222905adcf55b627489a55d11379893cac3ff66
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-06-30 01:17:31 +0000

    romanizado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,41 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+const RomanCoder = (num) => {
+let resultado = ""
+const valoresRomanos = [
+  
+  [1000, 'M'],
+  [1000, 'M'],
+  [1000, 'M'],
+  [1000, 'M'],
+  [900, 'CM'],
+  [500, 'D'],
+  [400, 'CD'],
+  [100, 'C'],
+  [90, 'XC'],
+  [50, 'L'],
+  [40, 'XL'],
+  [10, 'X'],
+  [9, 'IX'],
+  [5, 'V'],
+  [4, 'IV'],
+  [1, 'I']
+];
+for (const [valor,simbolo] of valoresRomanos) {
+  while (num >= valor ) {
+    num -= valor;
+    resultado += simbolo;
+  }
+}
+  document.getElementById("romanNum").innerText = resultado
+}
+document.querySelector(".btn").addEventListener("click",()=>{
+  let MyNum = prompt("Selecciona un número entre 1 y 3999");
+  if (isNaN(MyNum) || MyNum < 1 || MyNum > 3999) {
+  alert("Por favor ingresa un número válido entre 1 y 3999.");
+  return;
+}
+  RomanCoder(parseInt(MyNum))
+  document.getElementById("myNum").innerText = MyNum;
+})
